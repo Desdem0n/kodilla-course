2798476b6592b945c802b4bbf9746064963f570e
Desdem0n <desmilke@gmail.com>
2018-11-08 11:37:11 +0100

    first commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        double x = 23.4;
+        double y = 17.1;
+        Calculator calculator = new Calculator();
+        double sumXY = calculator.addAToB(x, y);
+        double subsXY = calculator.substractAFromB(x, y);
+        System.out.println("Sum of numbers: " + x + " i " + y + " equals " + sumXY);
+        System.out.println("difference of numbers: " + x + " i " + y + " equals " + subsXY);
     }
 }
